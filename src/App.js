7da6f70342147b7483c42bb17dfc757c02112d48
Sudharsan K <sudharsan_1985@live.in>
2025-03-16 19:06:25 +0800

    Updated the app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Changed again
+          Hi there! Changed to test the CI/CD pipeline with Travis CI and AWS Elastic Beanstalk.
         </p>
         <a
           className="App-link"
